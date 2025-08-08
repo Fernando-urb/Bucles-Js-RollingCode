841dc395e52b5fb4647dde0f4fc832ba6b2681af
fernando Urbano <fernando_urbano@outlook.com>
2025-08-08 20:27:05 -0300

    ejercicio 12

--- js/12-Bucles.js
+++ js/12-Bucles.js
@@ -1,1 +1,10 @@
 // Realiza un script que genere un número aleatorio entre 1 y 99
+
+let numeroRandom  ;
+
+do {
+  numeroRandom = Math.floor(Math.random() * 99) + 1; 
+  console.log(`El número es: ${numeroRandom}`);
+
+
+} while (numeroRandom <= 1);
