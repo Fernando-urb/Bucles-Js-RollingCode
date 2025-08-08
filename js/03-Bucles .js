d1a7815620c737839b04449fc6adc528690775cd
fernando Urbano <fernando_urbano@outlook.com>
2025-08-08 12:51:55 -0300

    ejercicio 3

--- js/03-Bucles .js
+++ js/03-Bucles .js
@@ -1,1 +1,12 @@
 // Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+let texto = prompt("ingrese un msj");
+const textoCompleto =[];
+
+while(texto !== null ){
+    textoCompleto.push(texto.trim())
+    texto = prompt("ingrese un msj")
+
+
+}
+document.write(textoCompleto.join("-"))
